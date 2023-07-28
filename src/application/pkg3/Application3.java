81eb66a5fdc376cfcd8143ca19953229a9ad4fca
antrohebsiba <antrohebsibaxe809@gmail.com>
2023-07-28 02:37:35 -1200

    hello

---
+++ src/application/pkg3/Application3.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package application.pkg3;
+
+/**
+ *
+ * @author ELCOT
+ */
+public class Application3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // variable declaration
+        //byte
+        byte a=19;
+        System.out.println(a);
+        long ph;
+        ph=6789943409l;
+        System.out.println(ph);
+        short no=6778;
+        System.out.println(no);
+        int x=46,y=78,z=-89;
+        System.out.println(x+y);
+        System.out.println(z);
+        System.out.println(x+y+z);
+        //boolean
+       boolean male=false;
+        System.out.println(male);
+        char spc=' ';
+        System.out.println(spc);
+        //string
+           String pswd="gfihskfh";
+           System.out.println(pswd);
+           String para="Elephant is the biggest animal in the world";
+           System.out.println(para);
+           //float
+           float call=67890.875634f;
+           System.out.println(call);
+           //char
+           char c1='1';
+           char c2='2';
+           System.out.println(c1+c2);
+           
+          
+           
+           
+                
+        
+        
+      
+        
+        
+    }
+    
+}
